1b0e924c1b5ae221f4db3935d2d4c92325abc70c
kehinde <bankolek1@gmail.com>
2021-03-25 11:04:53 +0100

    second commit

--- App.js
+++ App.js
@@ -1,127 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+export default class App extends Component {
+  state = {
+    myState:
+      'This is a text component, created using state data. It will change or updated on clicking it.',
   };
-
-  return (
-    // <SafeAreaView style={backgroundStyle}>
-    //   <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-    //   <ScrollView
-    //     contentInsetAdjustmentBehavior="automatic"
-    //     style={backgroundStyle}>
-    //     <Header />
-    //     <View
-    //       style={{
-    //         backgroundColor: isDarkMode ? Colors.black : Colors.white,
-    //       }}>
-    //       <Section title="Step One">
-    //          <Text style={styles.highlight}>App.js</Text> to change this
-    //         screen and then come back to see your edits.
-    //         this is my first app
-    //       </Section>
-    //       <Section title="See Your Changes">
-    //         <ReloadInstructions />
-    //       </Section>
-    //       <Section title="Debug">
-    //         <DebugInstructions />
-    //       </Section>
-    //       <Section title="Learn More">
-    //         Read the docs to discover what to do:
-    //       </Section>
-    //       <LearnMoreLinks />
-    //     </View>
-    //   </ScrollView>
-    // </SafeAreaView>
-    <View style={styles.container}>
-      <View style={{backgroundColor: 'blue', flex: 1}}>
-      <Text style={{fontSize: 18 , color:"white"}}>View Example</Text>
+  updateState = () => this.setState({myState: 'The state is updated'});
+  render() {
+    return (
+      <View style={style.container}>
+        <View style={style.blue}>
+          <Text>Hello</Text>
+        </View>
+        <View style={style.white}>
+          <Text>white</Text>
+        </View>
       </View>
-      <View style={{backgroundColor: 'red', flex: 1}} />
-     
-    </View>
-  );
-};
+    );
+  }
+}
 
-const styles = StyleSheet.create({
-  // sectionContainer: {
-  //   marginTop: 32,
-  //   paddingHorizontal: 24,
-  // },
-  // sectionTitle: {
-  //   fontSize: 24,
-  //   fontWeight: '600',
-  // },
-  // sectionDescription: {
-  //   marginTop: 8,
-  //   fontSize: 18,
-  //   fontWeight: '400',
-  // },
-  // highlight: {
-  //   fontWeight: '700',
-  // },
-
+const style = StyleSheet.create({
   container: {
+    backgroundColor: 'red',
+    height: '100%',
     flex: 1,
     flexDirection: 'row',
-    backgroundColor: 'yellow',
-    height:"100%"
   },
+  blue: {
+    backgroundColor: 'blue',
+    flex: 1,
+  },
+  white: {
+    backgroundColor: 'white',
+    flex: 1,
+  }
 });
-
-export default App;
